245897dc8c8b116fc8dd5975467dd5c2aff5f082
Pierrick Bottin <pierrickbottin@MacBook-Pro-de-Pierrick.local>
2020-03-03 12:49:48 +0100

    Upgrade JUnit to 4.13 & add new tests

--- src/main/java/com/codeworks/kata/App.java
+++ src/main/java/com/codeworks/kata/App.java
@@ -7,14 +7,30 @@ package com.codeworks.kata;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Some sample tests" );
 
         // Case 1 : 'abba'
         final String expectedCase1 = "abba";
         final String returnCase1 = findBiggestPalindrome("abba");
         System.out.println(String.format(
-                "Case 1 : 'abba' => %s return '%s' expected 'abba'",
-                "", findBiggestPalindrome("abba")
+                "Case 1 : 'abba' => %s ! return '%s' expected '%s'",
+                expectedCase1.equals(returnCase1) ? "SUCCESS" : "FAILED", returnCase1, expectedCase1
+        ));
+
+        // Case 2 : 'a'
+        final String expectedCase2 = "a";
+        final String returnCase2 = findBiggestPalindrome("a");
+        System.out.println(String.format(
+                "Case 2 : 'abba' => %s ! return '%s' expected '%s'",
+                expectedCase2.equals(returnCase2) ? "SUCCESS" : "FAILED", returnCase2, expectedCase2
+        ));
+
+        // Case 3 : 'paba'
+        final String expectedCase3 = "aba";
+        final String returnCase3 = findBiggestPalindrome("paba");
+        System.out.println(String.format(
+                "Case 3 : 'paba' => %s ! return '%s' expected '%s'",
+                expectedCase3.equals(returnCase3) ? "SUCCESS" : "FAILED", returnCase3, expectedCase3
         ));
 
     }
--- src/test/java/com/codeworks/kata/AppTest.java
+++ src/test/java/com/codeworks/kata/AppTest.java
@@ -1,25 +1,60 @@
 package com.codeworks.kata;
 
-import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
 import java.util.Date;
 
-public class AppTest extends TestCase {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AppTest {
     final Date start = new Date();
 
-    public AppTest( String testName ) {
-        super( testName );
+    @Test
+    public void T001_Simple() {
+        Assert.assertEquals("abba",  App.findBiggestPalindrome("abba"));
     }
 
-    public void test_simple_1() {
-        assertEquals("abba",  App.findBiggestPalindrome("abba"));
+    @Test
+    public void T002_Simple() {
+        Assert.assertEquals("aba",  App.findBiggestPalindrome("paba"));
     }
 
-    public void test_simple_2() {
-        assertEquals("aba",  App.findBiggestPalindrome("aba"));
+    @Test
+    public void T003_Empty() {
+        Assert.assertEquals("",  App.findBiggestPalindrome(""));
     }
 
-    public void test_simple_3() {
-        assertEquals("a",  App.findBiggestPalindrome("ab"));
+    @Test
+    public void T004_SingleCharacter() {
+        Assert.assertEquals("t",  App.findBiggestPalindrome("t"));
+    }
+
+    @Test
+    public void T005_Medium() {
+        Assert.assertEquals("azertytreza",  App.findBiggestPalindrome("tdexpazertytrezaskhbiyb"));
+    }
+
+    @Test
+    public void T006_Medium() {
+        Assert.assertEquals("kayak",  App.findBiggestPalindrome("kayakstkayak"));
+    }
+
+    @Test
+    public void T007_Medium() {
+        Assert.assertEquals("a",  App.findBiggestPalindrome("abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    @Test
+    public void T008_Long() {
+        Assert.assertEquals("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",  App.findBiggestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+    }
+
+    @Test
+    public void T009_Long() {
+        Assert.assertEquals("a",  App.findBiggestPalindrome("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"));
     }
 }
 
